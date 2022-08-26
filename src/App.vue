8f48ceddf795b871b5ba6eab48706356cef4590e
teacher_cheater <johnnykayfun@gmail.com>
2022-08-26 19:04:58 +0300

    add files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,120 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="container">
+    <div class="header">
+      <h1 class="header__title">To do list</h1>
+      <div class="header__add-task">+</div>
+    </div>
+    <div class="search">
+      <input class="search__text" type="text" placeholder="Поиск Имени, статуса или даты"/>
+      <p class="search__sort">Сортировать по:</p>
+    </div>
+    <div class="text">
+      <p class="subtext">Описание</p>
+      <div class="sort">
+        <p class="subtext">Статус</p>
+        <p class="subtext">Дата</p>
+      </div>
+      </div>
+    <div class="tasks">
+      <div class="task">
+        <input type="checkbox" name="checkbox" id="checkbox">
+        <p class="problem">TASK 01</p>
+        <div class="block-status">
+          <p class="status">Выполнено</p>
+          <p class="date">Дата</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
 
-nav {
-  padding: 30px;
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+<style scoped>
+  .container{
+    max-width: 1330px;
+    padding: 0 15px;
+    margin: 0 auto;
+  }
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .header{
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 40px;
+  }
+  .header__title{
+    font-weight: 700;
+    font-size: 24px;
+    line-height: 132%;
+    color: #16191D;
+  }
+  .header__add-task{
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: dimgray;
+  }
+  .search{
+    padding: 10px 40px;
+    display: flex;
+    justify-content: space-between;
+  }
+  input:active, :hover, :focus {
+       outline: 0;
+       outline-offset: 0;
+  }
+  .search__text{
+    width: 100%;
+    max-width: 300px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 132%;
+    color: #C4C4C4;
+    /*background: white;*/
+  }
+
+  .text{
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 40px;
+
+  }
+  .subtext{
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 132%;
+    color: #16191D;
+    border-left: 1px solid #C4C4C4;
+    padding: 0 20px;
+  }
+  .sort{
+    display: flex;
+  }
+  #checkbox{
+
+  }
+  .tasks{
+    /*display: flex;*/
+  }
+  .task{
+    display: flex;
+    padding: 20px 40px;
+    gap: 20px;
+    border-bottom: 1px solid #C4C4C4;
+    border-top: 1px solid #C4C4C4;
+  }
+  .problem{
+
+  }
+
 </style>
