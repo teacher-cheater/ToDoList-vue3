b372d336a568037078ca6d86a75f180cafb1e950
teacher_cheater <johnnykayfun@gmail.com>
2022-08-27 15:23:40 +0300

    add styles popup

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,6 @@
         <img src="./icons/search.svg" alt="search">
         <input class="search__text" type="text" placeholder="Поиск Имени, статуса или даты"/>
       </div>
-
       <p class="search__sort">Сортировать по: <span>date</span></p>
     </div>
     <div class="text">
@@ -36,7 +35,7 @@
       <div class="create__block">
         <div class="create__content">
           <h3 class="create__title">Создать новую задачу</h3>
-          <div class="create__close"></div>
+          <div class="create__close"><span class="create__close-icon"></span></div>
         </div>
         <div class="create__items">
           <p class="create__text">Описание</p>
@@ -57,45 +56,7 @@ export default {
 </script>
 
 <style scoped>
-  .create{
-    max-width: 400px;
-    padding: 40px 40px 50px 40px;
-    background: #FFFFFF;
-    border: 1px solid #DDE2E4;
-    box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
-    border-radius: 6px;
-  }
-  .create__content{
-    display: flex;
-    justify-content: space-between;
-    margin-bottom: 30px;
-  }
-  .create__title{
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 132%;
-    color: #16191D;
-  }
-  .create__close{
-    background: #314B99;
-    border-radius: 5px;
-  }
-  .create__text{
-    font-weight: 400;
-    font-size: 14px;
-    line-height: 14px;
-    color: #16191D;
-    margin-bottom: 5px;
-  }
-  .create__task{
-    background: #FFFFFF;
-    border: 1px solid #DDE2E4;
-    border-radius: 8px;
-    width: 100%;
-    padding: 16px 11px;
-    color: #000000;
-    opacity: 0.5;
-  }
+
   .container{
     max-width: 1330px;
     padding: 0 15px;
@@ -105,6 +66,7 @@ export default {
   *{
     margin: 0;
     padding: 0;
+    border: 0;
     box-sizing: border-box;
   }
 
@@ -167,7 +129,6 @@ export default {
     font-size: 14px;
     line-height: 132%;
     color: #C4C4C4;
-    /*background: white;*/
   }
 
   .text{
@@ -211,7 +172,6 @@ export default {
     padding: 20px 40px;
     border-bottom: 1px solid #C4C4C4;
     border-top: 1px solid #C4C4C4;
-
   }
 
   .block-status{
@@ -244,4 +204,83 @@ export default {
     padding: 0 0 0 20px;
     width: 150px;
   }
+  .create{
+    margin: auto;
+    max-width: 400px;
+    padding: 40px 40px 50px 40px;
+    background: #FFFFFF;
+    border: 1px solid #DDE2E4;
+    box-shadow: 0 25px 50px -12px rgba(0, 0, 0, 0.25);
+    border-radius: 6px;
+  }
+  .create__content{
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 30px;
+  }
+  .create__title{
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 132%;
+    color: #16191D;
+  }
+  .create__close{
+    background: #314B99;
+    border-radius: 5px;
+    width: 30px;
+    height: 30px;
+    position: relative;
+    cursor: pointer;
+  }
+  .create__close-icon{
+    background: #FFFFFF;
+    width: 18px;
+    height: 2px;
+    position: absolute;
+    top: 14px;
+    left: 6px;
+    transform: rotate(45deg);
+  }
+  .create__close-icon::before{
+    content: '';
+    transform: rotate(90deg);
+    background: #FFFFFF;
+    width: 18px;
+    height: 2px;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .create__items{
+    margin-bottom: 30px;
+  }
+  .create__text{
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 14px;
+    color: #16191D;
+    margin-bottom: 5px;
+  }
+  .create__task{
+    background: #FFFFFF;
+    border: 1px solid #DDE2E4;
+    border-radius: 8px;
+    width: 100%;
+    padding: 16px 11px;
+    color: #000000;
+    opacity: 0.5;
+  }
+  .create__btn{
+    display: flex;
+    background: #F0F5FF;
+    border-radius: 8px;
+    padding: 12px 40px;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 132%;
+    color: #314B99;
+    margin: 0 auto;
+    cursor: pointer;
+  }
+
 </style>
